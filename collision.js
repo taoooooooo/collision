e17f0d79bd2468b0245ee45a12219836717d5870
Johan Choi <johan.choi@education.nsw.gov.au>
2020-07-15 21:54:18 +1000

    gradient and collision point (common point)

--- collision.js
+++ collision.js
@@ -7,8 +7,8 @@
 		const INFINITY = Number.MAX_VALUE
 		const TIME_UNIT = 1 // 1 second
 
-		function makeBall (posX, posY, radius, velocityX, velocityY) {
-			return { x: posX, y: posY, radius: radius, vX: velocityX, vY: velocityY }
+		function makeBall (posX, posY, radius, velocityX, velocityY, mass) {
+			return { x: posX, y: posY, radius: radius, vX: velocityX, vY: velocityY, m: mass }
 		}
 
 function getMovingDirection(v) {
@@ -34,19 +34,61 @@ function distanceToWall (x_or_y, vx_or_vy, max_x_or_y) {
    }
 }
 
+// Simulate elements at 30 fps == 1/30 sec for each frame rendering
+// Therefore, we only need to compute all events within next 1/30 seconds.
+// calculate distance between 0 to 1/30 of a second to see if the balls collide
+// 1. get two line equations
+// 2. check if they cross (i.e., not parallel)
+// 3. compute time of when they cross
+// 4. if the time is between t0 <= cross-time <= t1(= t0 + 1/30 sec), then it means collision.
+function findGradientIntercept(ball) {
+	gradient = ball.vY/ball.vX // y = ax + b (a is known)
+	intercept = ball.y - gradient * ball.x
+  return [gradient, intercept]
+}
+
+function findCommonPoint(gradient1, intercept1, gradient2, intercept2) {
+	// 0 = (g1 - g2) x + (i1 -i2)
+	// (g1 -g2) x = -(i1 - i2)
+	// x = -(i1 - i2) / (g1 - g2)
+	x = -(intercept1 - intercept2) / (gradient1 - gradient2)
+	console.log((intercept1 - intercept2),  (gradient1 - gradient2))
+	y = gradient1 * x + intercept1 // y = ax + b
+	return [x, y]
+}
+
+console.log(findCommonPoint(1, 2, 1/2, 4))
+//
+// gradient1 = ball1.vy/ball1.vx // y = ax + b (a is known)
+// ball1.y = gradient1 * ball1.x + b
+// intercept1 = ball1.y - gradient1 * ball1.x
+//
+// gradient2 = ball2.vy/ball2.vx // y = ax + b (a is known)
+// ball2.y = gradient2 * ball2.x + b
+// intercept2 = ball2.y - gradient2 * ball2.x
+
+
+//
+// function distancebetweenBalls (ball1, ball2) {
+// 	// x: posX, y: posY, radius: radius, vX: velocityX, vY: velocityY, m: mass
+// 	//sqrt((x1-x2)^2 + (y1-y2)^2 - (r1 + r2)
+// 	distanceDots = Math.sprt(Math.pow(ball1.x - ball2.x, 2) + Math.pow(ball1.y - ball2.y, 2))
+// 	distanceBalls = distanceDots - (ball1.r + ball2.r)
+// }
+
+
 
 // TESTS
 console.log(distanceToWall(10, 10, 600))
-	//
-	// function distancetoBall (x_or_y, vx_or_vy, max_x_or_y) {
-	// 	switch (getM)
-	// }
+
 
 /* test balls
 b1 = makeBall(1,2,3,4,5) // hit right wall
 b2 = makeBall(4,6, 7, -10, 10) // hit left wall
 b3 = makeBall(34, 100, 122, 0,333) // hit no vertical wall
 INFINITY === distanceToWall(b3)
+
+distance
 */
 
 function timeToHitWall(x_or_y, vx_or_vy, max_x_or_y) {
