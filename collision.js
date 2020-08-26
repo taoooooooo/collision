5edf7de367c69d43aea33288c0a7d9e232c87109
Johan Choi <johan.choi@education.nsw.gov.au>
2020-08-26 20:51:59 +1000

    changed var to either const or let

--- collision.js
+++ collision.js
@@ -1,5 +1,4 @@
 /* Constants */
-
 // Walls
 const WALL_MAX_X = 900;
 const WALL_MAX_Y = 600;
@@ -57,8 +56,8 @@ function paintBall(ball) {
 ////////////////////////////////////////////
 function calcDistSquareCoeff(l1, l2, lX1, lX2) {
 	// use pythagoras
-	locDiff = l1 - l2;
-	velDiff = lX1 - lX2;
+const locDiff = l1 - l2;
+const	velDiff = lX1 - lX2;
 	return [Math.pow(locDiff, 2), //constant
 				2 * locDiff * velDiff, 		// t^1
 				Math.pow(velDiff, 2)];	//t^2
@@ -73,7 +72,7 @@ function makeBall(posX, posY, radius, velocityX, velocityY, n) {
 }
 
 function selectDirection(velocity) {
-	probability = Math.random();
+const	probability = Math.random();
 	if (0 <= probability && probability < 0.5) {
 		return velocity;
 	} else {
@@ -82,23 +81,23 @@ function selectDirection(velocity) {
 }
 
 function timeToCollide(ball1, ball2) {
-	coeffX = calcDistSquareCoeff(ball1.x, ball2.x, ball1.vX, ball2.vX);
-	coeffY = calcDistSquareCoeff(ball1.y, ball2.y, ball1.vY, ball2.vY);
+const	coeffX = calcDistSquareCoeff(ball1.x, ball2.x, ball1.vX, ball2.vX);
+const	coeffY = calcDistSquareCoeff(ball1.y, ball2.y, ball1.vY, ball2.vY);
 
 	//quadratics
-	c = coeffX[0] + coeffY[0] - Math.pow((ball1.radius + ball2.radius), 2);
-	b = coeffX[1] + coeffY[1];
-	a = coeffX[2] + coeffY[2];
+const	c = coeffX[0] + coeffY[0] - Math.pow((ball1.radius + ball2.radius), 2);
+const	b = coeffX[1] + coeffY[1];
+const	a = coeffX[2] + coeffY[2];
 
 	//console.log("****" + a + ':' + b + ':' + c)
-  discriminant = Math.pow(b, 2) - 4 * a * c
+const discriminant = Math.pow(b, 2) - 4 * a * c
   if (discriminant < 0) {
     //imaginary "roots"
     return  Number.MAX_VALUE;
   }
-	sqrtPart = Math.sqrt(discriminant);
-	t1 = (-b + sqrtPart) / (2 * a);
-	t2 = (-b - sqrtPart) / (2 * a);
+const	sqrtPart = Math.sqrt(discriminant);
+const	t1 = (-b + sqrtPart) / (2 * a);
+const	t2 = (-b - sqrtPart) / (2 * a);
 
 	if ((isSameTime(t1, 0)) || (isSameTime(t2, 0))) {
 		return 0;
@@ -110,10 +109,10 @@ function timeToCollide(ball1, ball2) {
 }
 
 function velocitiesAfterCollision(m1, v1, m2, v2) {
-  totalM = m1 + m2
+const totalM = m1 + m2
 
-  finalV1 = v1 * (m1 - m2) / totalM + (v2 * 2 * m2)/totalM;
-  finalV2 = (v1* 2 * m1) / totalM + v2 * (m2 - m1)/totalM;
+const finalV1 = v1 * (m1 - m2) / totalM + (v2 * 2 * m2)/totalM;
+const finalV2 = (v1* 2 * m1) / totalM + v2 * (m2 - m1)/totalM;
 
   return [finalV1, finalV2]
 }
@@ -129,9 +128,9 @@ function makeRandBall(max_x, max_y, max_radius, max_v, n) {
 }
 
 function areBallsOverlapped(ball1, ball2) {
- d = Math.sqrt(Math.pow((ball2.x - ball1.x), 2) +
+let d = Math.sqrt(Math.pow((ball2.x - ball1.x), 2) +
 							 Math.pow((ball2.y - ball1.y), 2));
- return (d < ball1.radius + ball2.radius);
+ return (d <= ball1.radius + ball2.radius);
 }
 
 function makeCollisionEvent(ball1, ball2) {
@@ -140,8 +139,8 @@ function makeCollisionEvent(ball1, ball2) {
 
 function updateVelocityAfterCollision(ball1, ball2) {
   //totalM = ball1.m + ball2.m;
-  [vX1, vX2] = velocitiesAfterCollision(ball1.m, ball1.vX, ball2.m, ball2.vX);
-  [vY1, vY2] = velocitiesAfterCollision(ball1.m, ball1.vY, ball2.m, ball2.vY);
+const [vX1, vX2] = velocitiesAfterCollision(ball1.m, ball1.vX, ball2.m, ball2.vX);
+const [vY1, vY2] = velocitiesAfterCollision(ball1.m, ball1.vY, ball2.m, ball2.vY);
 
   ball1.vX = vX1;
   ball1.vY = vY1;
@@ -152,8 +151,8 @@ function updateVelocityAfterCollision(ball1, ball2) {
 ////////////////////////////////////////////
 
 function createNonOverlappingBall(existingBalls, n) {
-    newBall = makeRandBall(900, 600, 10, 50, n);
-    for (var ball of existingBalls) {
+const newBall = makeRandBall(900, 600, 10, 50, n);
+    for (const ball of existingBalls) {
         if (areBallsOverlapped(ball, newBall)) {
             return createNonOverlappingBall(existingBalls, n);
         }
@@ -163,25 +162,25 @@ function createNonOverlappingBall(existingBalls, n) {
 
 function earlyCollisionEvents(balls) {
   // return array of events, which will occur the earliest time
-  var queue = new PriorityQueue({ comparator: function(event1, event2) {
+  let queue = new PriorityQueue({ comparator: function(event1, event2) {
     return event1.time - event2.time;
   }})
 
   	// build queue
-  	for (var i = 0; i < balls.length - 1; i++) {
-  		for	(var j = i + 1; j < balls.length; j++) {
-  				event = makeCollisionEvent(balls[i], balls[j]);
+  	for (let i = 0; i < balls.length - 1; i++) {
+  		for	(let j = i + 1; j < balls.length; j++) {
+  			const	event = makeCollisionEvent(balls[i], balls[j]);
   				queue.queue(event);
   		}
   	}
-    earlyTime = queue.peek().time;
-  	earliestEvents = [];
+  const earlyTime = queue.peek().time;
+  	let earliestEvents = [];
     do {
-      event = queue.dequeue()
+      const event = queue.dequeue()
       earliestEvents.push(event)
     } while (queue.length > 0 && queue.peek().time === earlyTime)
 
-    nextEarliestTime = (queue.lengh > 0) ? queue.peek().time: Number.MAX_VALUE;
+    const nextEarliestTime = (queue.lengh > 0) ? queue.peek().time: Number.MAX_VALUE;
 
   	return [earliestEvents, nextEarliestTime];
   }
@@ -199,8 +198,8 @@ function moveBall(ball, time) {
 ///////////////////////////////////////////
 function makeRandBalls(numberOfBalls) {
         //function will create an array of balls
-        ballArray = new Array(numberOfBalls);
-        for (i = 0; i < ballArray.length; i = i + 1) {
+        let ballArray = new Array(numberOfBalls);
+        for (let i = 0; i < ballArray.length; i = i + 1) {
             ballArray[i] = createNonOverlappingBall(ballArray.slice(0, i), i);
         }
         return ballArray;
@@ -208,31 +207,31 @@ function makeRandBalls(numberOfBalls) {
 
 function paintBalls(balls) {
 	clearCanvas();
-	for (var ball of balls) {
+	for (const ball of balls) {
 		paintBall(ball);
 	}
 }
 
 function moveBalls(balls) {
-  [events, nextEarliestTime] = earlyCollisionEvents(balls)
-  eventTime = events[0].time
+  const [events, nextEarliestTime] = earlyCollisionEvents(balls)
+  const eventTime = events[0].time
 
   if (isSameTime(eventTime, 0)) {
     changeForcesOfCollidingBalls(events);
   }
-  moveTime = isSameTime(eventTime, 0)
+  const moveTime = isSameTime(eventTime, 0)
   ? Math.min(nextEarliestTime, 1)
   : Math.min(eventTime, 1);
 
-	for (var ball of balls) {
+	for (const ball of balls) {
 		 moveBall(ball, moveTime)
    }
 }
 
 async function loop(seconds) {
-	balls = makeRandBalls(10);
+	const balls = makeRandBalls(10);
 	// repeats here
-	durationMs = 1000 * seconds;
+	let durationMs = 1000 * seconds;
 	while (0 < durationMs) {
 		paintBalls(balls);
 		moveBalls(balls);
@@ -271,7 +270,7 @@ function distanceToWall(x_or_y, vx_or_vy, max_x_or_y) {
 }
 
 function timeToHitWall(x_or_y, vx_or_vy, max_x_or_y) {
-  distance = distanceToWall(x_or_y, vx_or_vy, max_x_or_y);
+  const distance = distanceToWall(x_or_y, vx_or_vy, max_x_or_y);
   // console.log("1 " + distance)
   if (distance === Number.MAX_VALUE) {
     return Number.MAX_VALUE;
